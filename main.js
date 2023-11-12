30b1c5abab46f4b58a56ec71ed7439d08771da71
krati <craticat@gmail.com>
2023-11-13 04:16:39 +0500

    rework zuma algoritm

--- main.js
+++ main.js
@@ -11,10 +11,9 @@ let brokenLine = []
 let balls = []
 
 const DEFAULT_RADIUS = 10
-
 balls.push({positionOnLine: 0, radius: DEFAULT_RADIUS})
-balls.push({positionOnLine: 0, radius: DEFAULT_RADIUS})
-balls.push({positionOnLine: 0, radius: DEFAULT_RADIUS})
+balls.push({positionOnLine: 100, radius: DEFAULT_RADIUS})
+balls.push({positionOnLine: 400, radius: DEFAULT_RADIUS})
 balls.push({positionOnLine: 0, radius: DEFAULT_RADIUS})
 balls.push({positionOnLine: 0, radius: DEFAULT_RADIUS})
 
@@ -31,10 +30,6 @@ brokenLine.push({x: 250, y: 250})
 brokenLine.push({x: 300, y: 250})
 brokenLine.push({x: 350, y: 200})
 
-function vec2(x = 0, y = 0) {
-    return { x, y }
-}
-
 function interpolate(v1, v2, t) {
     return v1 + (v2 - v1) * t
 }
@@ -43,16 +38,46 @@ function quadraticBezier(v1, v2, p3, t) {
     return t * (t * (p3 - v2 * 2 + v1) + 2 * (v2 - v1)) + v1
 };
 
+function vec2(x = 0, y = 0) {
+    return { x, y }
+}
+
+function vec2sub(v1, v2) {
+    return {
+        x: v1.x - v2.x,
+        y: v1.y - v2.y
+    }
+}
+function vec2add(v1, v2) {
+    return {
+        x: v1.x + v2.x,
+        y: v1.y + v2.y
+    }
+}
+
+function vec2dotProduct(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y 
+}
+
+function vec2normal(v1) {
+    let len = Math.hypot(v1.x, v1.y)
+    return {
+        x: v1.x / len,
+        y: v1.y / len
+    }
+}
+
 function vec2distance(v1, v2) {
     return Math.hypot((v2.x - v1.x), (v2.y - v1.y))
 }
 
-function vec2interpolate(v1, v2) {
+function vec2interpolate(v1, v2, t) {
     return {
         x: interpolate(v1.x, v2.x, t),
         y: interpolate(v1.y, v2.y, t)
     }
 }
+
 function vec2quadraticBezier(v1, v2, p3, t) {
     return {
         x: quadraticBezier(v1.x, v2.x, p3.x, t),
@@ -67,69 +92,60 @@ function asd(points, circles) {
     let pointIndex = 1
     
     let lastResult = null
-    let line = [{x: 0, y: 0}, brokenLine[0]]
+    let line = [null, brokenLine[0]]
     
     let sumLength = 0
     while (pointIndex < points.length && circleIndex < circles.length) {
         let circle = circles[circleIndex]
         let point = points[pointIndex]
+
+        if (lastResult === null && circle.positionOnLine < 0) {
+            circleIndex += 1
+            continue
+        }
         
         line = [line[1], point]
     
-        let len = Math.hypot((line[1].x - line[0].x), (line[1].y - line[0].y))
-    
-        if (lastResult == null && circle.positionOnLine < 0) {
-            circleIndex += 1
-        } else if (len != 0 && sumLength + len > circle.positionOnLine) {
-            let mayberesult = null
-            if (sumLength <= circle.positionOnLine) {
-                let t = (circle.positionOnLine - sumLength) / len;
-                mayberesult = {x: line[0].x + (line[1].x - line[0].x) * t, y: line[0].y + (line[1].y - line[0].y) * t}
-            }
-            let needOffset = 0
-            if (lastResult !== null) {
-                if (mayberesult !== null) {
-                    //tut kakaya to error
-                    let templen = Math.hypot((lastResult.pos.x - mayberesult.x), (lastResult.pos.y - mayberesult.y))
-                    console.log(templen)
-                    if (lastResult.circle.positionOnLine + lastResult.needOffset >= circle.positionOnLine || templen <= circle.radius + lastResult.circle.radius) {
-                        mayberesult = null
-                    }
-                }
-                let templen = Math.hypot((lastResult.pos.x - line[1].x), (lastResult.pos.y - line[1].y))
-                if (mayberesult === null && templen > circle.radius + lastResult.circle.radius) {
-                    let b = {x: line[1].x - line[0].x, y: line[1].y - line[0].y}
-                    let c = {x: lastResult.pos.x - line[0].x, y: lastResult.pos.y - line[0].y}
-                    let root = (c.x * b.x + c.y * b.y) / (b.x * b.x + b.y * b.y)
-                    let proj = {x: line[0].x + (line[1].x - line[0].x) * root, y: line[0].y + (line[1].y - line[0].y) * root}
-                    
-                    let len0 = Math.hypot((proj.x - lastResult.pos.x), (proj.y - lastResult.pos.y))
-                    let len1 = circle.radius + lastResult.circle.radius
-                    let len2 = Math.sqrt(Math.abs(len1 * len1 - len0 * len0))
-                    
-                    let len = Math.hypot((line[0].x - proj.x), (line[0].y - proj.y))
-                    let lpnormal = {x: (line[0].x - proj.x) / len , y: (line[0].y - proj.y) / len}
-    
-                    let rootLinePoint = (line, point) => {
-                        let result = (point.y - line[0].y) / (line[1].y - line[0].y)
-                        if (isNaN(result)) {
-                            result = (point.x - line[0].x) / (line[1].x - line[0].x)
-                        }
-                        return result
-                    }
-                    let flag = rootLinePoint(line, proj) > 0 ? -1 : 1
-                    mayberesult = {x: proj.x + lpnormal.x * len2 * flag, y: proj.y + lpnormal.y * len2 * flag}
-                    needOffset = (sumLength + len * rootLinePoint(line, mayberesult)) - circle.positionOnLine
-                }
-            }
+        let len = vec2distance(line[0], line[1]);
+
+        let isgoodline = len && sumLength + len > circle.positionOnLine
+
+        let mayberesult = null
+        let needOffset = 0
+        
+        if (isgoodline) {
+            mayberesult = vec2interpolate(line[0], line[1], (circle.positionOnLine - sumLength) / len)
+        }
+
+        if (isgoodline && lastResult) {
+            let iscorrect = true
+            iscorrect &&= lastResult.circle.positionOnLine + lastResult.needOffset < circle.positionOnLine
+            iscorrect &&= vec2distance(lastResult.pos, mayberesult) > circle.radius + lastResult.circle.radius
             
-            if (mayberesult !== null) {
-                result[circleIndex] = {circle: circle, pos: mayberesult, needOffset: needOffset}
-                lastResult = result[circleIndex]
-                circleIndex += 1
-                line = [line[1], line[0]]
-                continue
+            if (!iscorrect) {
+                mayberesult = null
             }
+            if (!iscorrect && vec2distance(lastResult.pos, line[1]) > circle.radius + lastResult.circle.radius) {
+                let b = vec2sub(line[1], line[0])
+                let c = vec2sub(lastResult.pos, line[0])
+                let projRoot = vec2dotProduct(c, b) / vec2dotProduct(b, b)
+                let proj = vec2interpolate(line[0], line[1], projRoot)
+                
+                let len0 = vec2distance(lastResult.pos, proj)
+                let len1 = circle.radius + lastResult.circle.radius
+                let len2 = Math.sqrt(Math.abs(len1 * len1 - len0 * len0))
+                
+                mayberesult = vec2interpolate(line[0], line[1], projRoot + (len2 / len))
+                needOffset = sumLength + len * (projRoot + (len2 / len)) - circle.positionOnLine
+            }
+        }
+            
+        if (mayberesult !== null) {
+            result[circleIndex] = {circle: circle, pos: mayberesult, needOffset: needOffset}
+            lastResult = result[circleIndex]
+            circleIndex += 1
+            line = [line[1], line[0]]
+            continue
         }
     
         sumLength += len
@@ -155,7 +171,6 @@ let drawLoop = () => {
         context.arc(c.pos.x, c.pos.y, c.circle.radius, 0, Math.PI * 2)
         context.stroke()
     });
-    console.log(res)
     requestAnimationFrame(drawLoop)
 }
 
@@ -170,14 +185,14 @@ function quadraticBezierConverter(points) {
         let second = p
         let third = points[Math.min(points.length - 1, i + 1)]
         curves.push([
-            {x: first.x + (second.x - first.x) * t, y: first.y + (second.y - first.y) * t},
+            vec2interpolate(first, second, t),
             second,
-            {x: second.x + (third.x - second.x) * t, y: second.y + (third.y - second.y) * t}
+            vec2interpolate(second, third, t)
         ])
     })
     
     let result = [];
-    let quality = 1;
+    let quality = 10;
 
     curves.forEach( curve => {
         let sumlen = vec2distance(curve[0], curve[1]) + vec2distance(curve[1], curve[2])
@@ -192,4 +207,4 @@ function quadraticBezierConverter(points) {
     return result
 }
 
-brokenLine = quadraticBezierConverter(brokenLine)
+//brokenLine = quadraticBezierConverter(brokenLine)
